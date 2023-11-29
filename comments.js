3100e7bdf0dd8c077d011515a55b49b784c91bb3
Rogier Dijkman <40334679+azurekid@users.noreply.github.com>
2023-11-29 14:38:00 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,4 +1,90 @@
 // Create webserver
+
+// import http module
+const http = require('http');
+
+// define the request handling function
+const requestHandler = (request, response) => {
+    response.end('Hello World');
+}
+
+// create the server
+const server = http.createServer(requestHandler);
+
+// start the server
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+// Create webserver
+// Import http module
+const http = require('http');
+
+// Define the request handling function
+const requestHandler = (request, response) => {
+    response.end('Hello World');
+}
+
+// Create the server
+const server = http.createServer(requestHandler);
+
+// Start the server
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+// Create webserver
+// Import http module
+const http = require('http');
+
+// Define the request handling function
+const requestHandler = (request, response) => {
+    response.end('Hello World');
+}
+
+// Create the server
+const server = http.createServer(requestHandler);
+
+// Start the server
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+// Create webserver
+// Import http module
+const http = require('http');
+
+// Define the request handling function
+const requestHandler = (request, response) => {
+    response.end('Hello World');
+}
+
+// Create the server
+const server = http.createServer(requestHandler);
+
+// Start the server
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+// Create webserver
+// Import http module
+const http = require('http');
+
+// Define the request handling function
+const requestHandler = (request, response) => {
+    response.end('Hello World');
+}
+
+// Create the server
+const server = http.createServer(requestHandler);
+
+// Start the server
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+// Create webserver
 // Import http module
 const http = require('http');
 
