1364e9dcc1a62cf7eaab23c6ef1f128fb2e1f8d2
Rogier Dijkman <40334679+azurekid@users.noreply.github.com>
2023-11-29 14:39:49 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,102 +1,79 @@
-// Create webserver
+// Create webserver that serves the comments.json file
+// and allows users to post to it
 
-// import http module
-const http = require('http');
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var querystring = require('querystring');
 
-// define the request handling function
-const requestHandler = (request, response) => {
-    response.end('Hello World');
+var COMMENTS_FILE = 'comments.json';
+
+// Load comments from file
+function loadComments(callback) {
+  fs.readFile(COMMENTS_FILE, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    callback(JSON.parse(data));
+  });
 }
 
-// create the server
-const server = http.createServer(requestHandler);
+// Save comments to file
+function saveComments(comments, callback) {
+  fs.writeFile(COMMENTS_FILE, JSON.stringify(comments), function(err) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    callback();
+  });
+}
 
-// start the server
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
+// Handle GET requests
+function handleGet(request, response) {
+  var urlParts = url.parse(request.url, true);
+  var query = urlParts.query;
+  var path = urlParts.pathname;
+  if (path === '/comments') {
+    loadComments(function(comments) {
+      response.setHeader('Content-Type', 'application/json');
+      response.end(JSON.stringify(comments));
+    });
+  } else {
+    response.statusCode = 404;
+    response.end('Not found');
+  }
+}
+
+// Handle POST requests
+function handlePost(request, response) {
+  var body = '';
+  request.on('data', function(data) {
+    body += data;
+  });
+  request.on('end', function() {
+    var comment = querystring.parse(body);
+    loadComments(function(comments) {
+      comments.push(comment);
+      saveComments(comments, function() {
+        response.statusCode = 201;
+        response.end('Created');
+      });
+    });
+  });
+}
+
+// Create web server
+var server = http.createServer(function(request, response) {
+  if (request.method === 'GET') {
+    handleGet(request, response);
+  } else if (request.method === 'POST') {
+    handlePost(request, response);
+  }
 });
 
-// Create webserver
-// Import http module
-const http = require('http');
-
-// Define the request handling function
-const requestHandler = (request, response) => {
-    response.end('Hello World');
-}
-
-// Create the server
-const server = http.createServer(requestHandler);
-
-// Start the server
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
-
-// Create webserver
-// Import http module
-const http = require('http');
-
-// Define the request handling function
-const requestHandler = (request, response) => {
-    response.end('Hello World');
-}
-
-// Create the server
-const server = http.createServer(requestHandler);
-
-// Start the server
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
-
-// Create webserver
-// Import http module
-const http = require('http');
-
-// Define the request handling function
-const requestHandler = (request, response) => {
-    response.end('Hello World');
-}
-
-// Create the server
-const server = http.createServer(requestHandler);
-
-// Start the server
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
-
-// Create webserver
-// Import http module
-const http = require('http');
-
-// Define the request handling function
-const requestHandler = (request, response) => {
-    response.end('Hello World');
-}
-
-// Create the server
-const server = http.createServer(requestHandler);
-
-// Start the server
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
-
-// Create webserver
-// Import http module
-const http = require('http');
-
-// Define the request handling function
-const requestHandler = (request, response) => {
-    response.end('Hello World');
-}
-
-// Create the server
-const server = http.createServer(requestHandler);
-
-// Start the server
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
+// Start web server
+server.listen(8080, function() {
+  console.log('Server listening on port 8080');
 });
